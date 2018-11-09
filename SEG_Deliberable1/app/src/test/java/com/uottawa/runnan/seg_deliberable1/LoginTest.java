62f71def147d0439bcefc0ccca0b16e1ce56b72c
Dolores <dolores@RunnandeMacBook-Air.local>
2018-11-09 18:55:27 -0500

    add SEG_Deliverable2

---
+++ SEG_Deliberable1/app/src/test/java/com/uottawa/runnan/seg_deliberable1/LoginTest.java
@@ -0,0 +1,16 @@
+package com.uottawa.runnan.seg_deliberable1;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+public class LoginTest {
+    @Test
+    public void nameIsNotEmpty() throws Exception{
+
+    }
+}
